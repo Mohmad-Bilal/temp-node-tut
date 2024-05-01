be14e945258c47eb18518dd267d78d68189cbd92
Mohammad Bilal <mhd.mhdb2.2017@gmail.com>
2024-05-01 22:50:53 +0300

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,13 @@
+//Modules 
+
+const names = require('./4-names.js');
+const sayHi = require('./5-utils.js');
+const info = require('./6-alternatev-flavor.js')
+require('./7-mind-gernade.js')
+/onsole.log(names);
+
+console.log(info);
+
+sayHi('nero');
+sayHi(names.john);
+sayHi(names.peter);
